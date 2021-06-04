124b3fa8c63307367d885e3c2f23ec17e7c59b3f
JoandersonPaiva <Joandersonpaiva@gmail.com>
2021-06-04 05:00:13 -0300

    databases

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('livraria','superuser','VGvelBuwzvNtjOYk',{
+const connection = new Sequelize('livraria','livraria','User1234',{
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-03:00'
